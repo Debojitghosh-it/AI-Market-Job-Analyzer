ec0ed6b761bf09439416b4d83980ea451dcb9e88
Aaditya Parashar <ghoshdebojit418@gmail.com>
2026-06-22 16:46:28 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.title("AI Job Market Analyzer")
+
+df = pd.read_csv("cleaned_ai_jobs.csv")
+
+st.subheader("Dataset Overview")
+st.write(df.head())
+
+# Top job roles
+st.subheader("Top Job Roles")
+
+top_jobs = df["Job_Title"].value_counts().head(10)
+
+fig, ax = plt.subplots(figsize=(10, 5))
+top_jobs.plot(kind="bar", ax=ax)
+
+plt.xticks(rotation=45)
+
+st.pyplot(fig)
